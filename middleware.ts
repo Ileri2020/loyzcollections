0551aa3d7b19a8af403fa43847ebf17090a9c831
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2026-06-29 08:47:45 +0100

    fake middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+// export { auth as middleware } from "./auth";
+// export const runtime = "nodejs";
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/about/:path*',
+}
